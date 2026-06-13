5fa7de3a266b70d86b2e91e5b52b7643d5f26067
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2026-06-13 17:59:13 -0300

    cria rotas no front pra prof

--- frontend/src/pages/Prof/SidebarProfessor.tsx
+++ frontend/src/pages/Prof/SidebarProfessor.tsx
@@ -1,13 +1,12 @@
-export function Sidebar(){
-    return(
+import { Link } from "react-router-dom";
+
+export function Sidebar() {
+    return (
         <nav className="sidebar">
-            <a href="/Prof/Dashboard">Dashboard</a>
-            <a href="/Prof/Notas">Notas</a>
-            <a href="/Prof/Frequencia">Frequência</a>
-            <a href="/Prof/Avisos">Avisos</a>
-    </nav>
+            <Link to="/prof/dashboard">Dashboard</Link>
+            <Link to="/prof/notas">Notas</Link>
+            <Link to="/prof/frequencia">Frequência</Link>
+            <Link to="/prof/avisos">Avisos</Link>
+        </nav>
     );
 }
-
-
-
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -5,6 +5,10 @@ import Prof from "../pages/Prof/page";
 import Responsavel from "../pages/Responsavel/page";
 import AdminEscolar from "../pages/AdminEscolar/page";
 import AdminSistema from "../pages/AdminSistema/page";
+import { Avisos } from "../pages/Prof/Avisos";
+import { Frequencia } from "../pages/Prof/Frequencia";
+import { Notas } from "../pages/Prof/Notas";
+import { Dashboard } from "../pages/Prof/Dashboard";
 
 export default function AppRoutes() {
   return (
@@ -15,6 +19,10 @@ export default function AppRoutes() {
         <Route path="/admin-sistema" element={<AdminSistema />} />
         <Route path="/prof" element={<Prof />} />
         <Route path="/responsavel" element={<Responsavel />} />
+        <Route path="/prof/dashboard" element={<Dashboard />} />
+        <Route path="/prof/notas" element={<Notas />} />
+        <Route path="/prof/frequencia" element={<Frequencia />} />
+        <Route path="/prof/avisos" element={<Avisos />} />
       </Routes>
     </BrowserRouter>
   );
