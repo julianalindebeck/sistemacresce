da2bb6469931d8628b368ffddcf74e5cb161c329
Fernanda Coutinho <fernanda.coutinho@estudante.ufjf.br>
2026-06-13 16:26:31 -0300

    adicioan autenticação e verificação de credenciais

--- backend/src/classes/Usuario.ts
+++ backend/src/classes/Usuario.ts
@@ -1,13 +1,17 @@
+import { TipoUsuario } from '..//usuario/tipo-usuario';
+
 export abstract class Usuario{
     id: number;
     nome: String;
     email: String;
     senha: String;
+    tipo: TipoUsuario;
     
-    constructor(id: number, nome: String, email: String, senha: String){
+    constructor(id: number, nome: String, email: String, senha: String, tipo: TipoUsuario){
         this.id = id;
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+        this.tipo = tipo;
     }
 }
