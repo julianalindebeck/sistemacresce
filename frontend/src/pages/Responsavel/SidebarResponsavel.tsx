1564b6a791875335b37c5b859dc9a59524b0b4a3
Juliana Lindebeck <juliana.lindebeck@estudante.ufjf.br>
2026-06-20 16:06:31 -0300

    adiciona icons nos menus laterais e modifica css

--- frontend/src/pages/Responsavel/SidebarResponsavel.tsx
+++ frontend/src/pages/Responsavel/SidebarResponsavel.tsx
@@ -1,16 +1,41 @@
 import { Link, NavLink } from "react-router-dom";
+import { FiHome, FiBookOpen, FiBarChart2, FiBell, FiFileText, FiLogOut } from "react-icons/fi";
+
 import "../../styles/sidebar.css";
 
 export function SidebarResponsavel() {
     return (
         <nav className="sidebar">
-            <p>Menu</p>
-            <NavLink to="/responsavel" end>Inicio</NavLink>
-            <NavLink to="/responsavel/notas">Notas</NavLink>
-            <NavLink to="/responsavel/frequencia">Frequencia</NavLink>
-            <NavLink to="/responsavel/avisos">Avisos</NavLink>
-            <NavLink to="/responsavel/relatoriofinal">Relatorio Final</NavLink>
-            <Link to="/login">Sair</Link>
+            <p>MENU</p>
+            <NavLink to="/responsavel" end>
+                <FiHome />
+                <span>Início</span>
+            </NavLink>
+
+            <NavLink to="/responsavel/notas">
+                <FiBookOpen />
+                <span>Notas</span>
+            </NavLink>
+
+            <NavLink to="/responsavel/frequencia">
+                <FiBarChart2 />
+                <span>Frequência</span>
+            </NavLink>
+
+            <NavLink to="/responsavel/avisos">
+                <FiBell />
+                <span>Avisos</span>
+            </NavLink>
+
+            <NavLink to="/responsavel/relatoriofinal">
+                <FiFileText />
+                <span>Relatório Final</span>
+            </NavLink>
+
+            <Link to="/login" className="logout">
+                <FiLogOut />
+                <span>Sair</span>
+            </Link>
         </nav>
     );
 }
