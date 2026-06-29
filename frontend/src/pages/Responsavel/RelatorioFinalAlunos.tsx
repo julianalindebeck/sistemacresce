14c66ac67283c79867dcbf59c1643475de3084bc
Breno Loureiro <brenoloupaula@gmail.com>
2026-06-29 17:42:25 -0300

    cria a tela do relatorio na pagina do responsavel (falta gerar o pdf)

--- frontend/src/pages/Responsavel/RelatorioFinalAlunos.tsx
+++ frontend/src/pages/Responsavel/RelatorioFinalAlunos.tsx
@@ -1,10 +1,25 @@
-import { SidebarResponsavel } from "./SidebarResponsavel";
+import { SidebarResponsavel } from "././SidebarResponsavel";
+import "../AdminEscolar/RelatorioFinalAdminEscolar.css"
+import relatorio from "../../assets/relatorio.png";
 
 export function RelatorioFinalAlunos(){
     return (
-            <>
-            <h1>Relatorio</h1>
-            <SidebarResponsavel></SidebarResponsavel>
-            </>
+        <>
+        <div className="pagRelatorio">
+            <div className="topo">
+                <h1>Relatório Final</h1>
+      
+            </div>
+            <div className="imagem">
+                <img src={relatorio} alt="imagem do relatorio" className="relatorio"/>
+            </div>
+            <div className="botao-gerar-relatorio">
+                <button>Baixar Relatório</button>
+            </div>
+
+        </div>
+
+        <SidebarResponsavel></SidebarResponsavel>
+        </>
     );
 }
